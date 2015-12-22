a7551d0b5bad9555fccb028ed25557638e9fae80
Dahun Lee <dahun0428@gmail.com>
2015-12-23 02:14:49 +0900

    rewrite some code for No hard-coding

--- grade.py
+++ grade.py
@@ -7,28 +7,48 @@ def do_only_one():
   os.system("curl -o ./meaningless.txt --cookie ./cookie.txt --cookie-jar ./cookie.txt --user-agent Mozilla/4.0 -X POST --data \"j_username="+username+"&j_password="+password+"&last_login_id_save=1\" http://mpovis.postech.ac.kr/mpovis/login.do > ./trash.txt 2>&1")  
 
 def dothis(number,wait):
-  os.system("/usr/bin/curl -o ./mygrade.txt --cookie ./cookie.txt --cookie-jar ./cookie.txt --user-agent Mozilla/4.0 \"http://mpovis.postech.ac.kr/mpovis/zcm/ZCMW6011.do?pType=normal_search&iPeryr=2015&iPerid=092\" >> ./trash.txt 2>&1")
+  year = time.strftime ("%Y", time.localtime ())
+
+  os.system("/usr/bin/curl -o ./mygrade.txt --cookie ./cookie.txt --cookie-jar ./cookie.txt --user-agent Mozilla/4.0 \"http://mpovis.postech.ac.kr/mpovis/zcm/ZCMW6011.do?pType=normal_search&iPeryr="+year+"&iPerid=092\" >> ./trash.txt 2>&1")
 
   f = open("./mygrade.txt","r")
-  i=356
-  line= f.readlines()
-  for j in xrange(int(number)):
-    subject = line[i].replace('<td>',' ').replace('</td>',' ').replace('\t\t\t\t\t',' ')
-    grade = line[i+6].replace('<td>',' ').replace('</td>',' ').replace('\t\t\t\t\t\t\t',' ')
-    print subject,grade
-    if wait in subject and wait != '':
-      if re.findall(r'[A-Z]', grade):
-        print("\a")
+
+  while 1:
+    line = f.readline()
+    if "Completed" in line:
+      break
+
+  while 1:
+    line = f.readline()
+
+    if re.findall (r"[A-Z]{4}[0-9]{3}",line):
+      courseNo = line.replace('<td>','').replace('</td>','').replace('\t\t\t\t\t\t\t','')
+      f.readline()
+      subject = f.readline().replace('<td>','').replace('</td>','').replace('\t\t\t\t\t','').replace('\n','')
+    
+    elif "Credit" in line:
+      grade = f.readline().replace('<td>','').replace('</td>','').replace('\t','')
+      print subject, courseNo
+      
+      if re.findall (r"[ABCDFISU]",grade):
+        print "\tGrade: ", grade
+      else:
+        print ""
+        
+
+    elif "</html>" in line:
+      break
 
 
-    i+= 17
 
 if __name__ == "__main__":
-  os.system ("rm cookie.txt;rm meaningless.txt; rm ./trash.txt")
+  os.system ("/bin/rm cookie.txt 2>/dev/null ;/bin/rm meaningless.txt 2>/dev/null; /bin/rm ./trash.txt 2>/dev/null")
   username = raw_input ("input your POVIS ID: ")
   password = getpass.getpass ("input your password: ")
-  number = raw_input ("the number of subjects: ")
-  wait = raw_input ("If waiting: ")
+#  number = raw_input ("the number of subjects: ")
+#  wait = raw_input ("If waiting: ")
+  number = 1
+  wait = 1
   do_only_one ()
   while 1:
     print time.strftime ("%Y-%m-%d %A %I:%M", time.localtime ())
